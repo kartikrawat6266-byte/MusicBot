c66fdf46a0611489027032799eb8add420da91ee
kartikrawat6266-byte <kartikrawat6266@gmail.com>
2026-05-23 19:05:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,66 @@
 import os
 import yt_dlp
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
 
 BOT_TOKEN = os.getenv("8937999210:AAFMY6svP5Nc6k0stAt-HcFE0LcHOwlobAM")
 
-async def start_music(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.message.text
+if not BOT_TOKEN:
+    print("❌ BOT_TOKEN not found!")
+    exit()
 
-    msg = await update.message.reply_text("🎵 Searching Song...")
+async def music_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    song_name = update.message.text
+
+    searching = await update.message.reply_text(
+        f"🎵 Searching:\n{song_name}"
+    )
 
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': '%(title)s.%(ext)s',
-        'quiet': True,
-        'noplaylist': True,
+        "format": "bestaudio/best",
+        "outtmpl": "%(title)s.%(ext)s",
+        "quiet": True,
+        "noplaylist": True,
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(f"ytsearch1:{query}", download=True)
+            info = ydl.extract_info(
+                f"ytsearch1:{song_name}",
+                download=True
+            )
 
-            video = info['entries'][0]
+            video = info["entries"][0]
+
             filename = ydl.prepare_filename(video)
 
         await update.message.reply_audio(
-            audio=open(filename, 'rb'),
-            title=video['title'],
+            audio=open(filename, "rb"),
+            title=video["title"],
             performer="MusicBot"
         )
 
-        await msg.delete()
-
         os.remove(filename)
 
+        await searching.delete()
+
     except Exception as e:
-        await update.message.reply_text(f"❌ Error:\n{e}")
+        await update.message.reply_text(
+            f"❌ Error:\n{e}"
+        )
 
 app = Application.builder().token(BOT_TOKEN).build()
 
 app.add_handler(
-    MessageHandler(filters.TEXT & ~filters.COMMAND, start_music)
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        music_handler
+    )
 )
 
 print("✅ Music Bot Started")
