88e28e2c26b0099b9f94d4ee80688ceaf2cb76c8
kartikrawat6266-byte <kartikrawat6266@gmail.com>
2026-05-23 18:56:16 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+import os
+import yt_dlp
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start_music(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.message.text
+
+    msg = await update.message.reply_text("🎵 Searching Song...")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s',
+        'quiet': True,
+        'noplaylist': True,
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(f"ytsearch1:{query}", download=True)
+
+            video = info['entries'][0]
+            filename = ydl.prepare_filename(video)
+
+        await update.message.reply_audio(
+            audio=open(filename, 'rb'),
+            title=video['title'],
+            performer="MusicBot"
+        )
+
+        await msg.delete()
+
+        os.remove(filename)
+
+    except Exception as e:
+        await update.message.reply_text(f"❌ Error:\n{e}")
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, start_music)
+)
+
+print("✅ Music Bot Started")
+
+app.run_polling(drop_pending_updates=True)
