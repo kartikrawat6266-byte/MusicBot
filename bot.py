37062d06836ac73cbb3743d194b58f98693f1d34
kartikrawat6266-byte <kartikrawat6266@gmail.com>
2026-05-24 00:15:37 +0530

    Final Update Bot

--- bot.py
+++ bot.py
@@ -4,10 +4,12 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 import yt_dlp
 
-API_ID = 35140329
-API_HASH = "011f638e4acadee178c59afffc80193d"
-BOT_TOKEN = "8917775888:AAHvVcNK1RG7ty6bR7OIRmCSGJcKAPWjirw"
+# ENV VARIABLES
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# BOT
 app = Client(
     "music_bot",
     api_id=API_ID,
@@ -15,19 +17,30 @@ app = Client(
     bot_token=BOT_TOKEN
 )
 
+# DOWNLOAD FOLDER
 os.makedirs("downloads", exist_ok=True)
 
-
+# START
 @app.on_message(filters.command("start"))
 async def start(client, message: Message):
 
     await message.reply_text(
-        "🎵 Music Bot Active!\n\n"
-        "Use:\n"
-        "/play song name"
+        """
+🎵 Premium Music Bot Active!
+
+⚡ Fast Download
+🎧 HQ Audio
+📥 Instant Upload
+
+👑 Owner:
+@BeStChEaT_OwNeR
+
+Use:
+/play song name
+"""
     )
 
-
+# PLAY
 @app.on_message(filters.command("play"))
 async def play(client, message: Message):
 
@@ -55,13 +68,22 @@ async def play(client, message: Message):
 
         url = f"https://youtube.com/watch?v={song['id']}"
 
-        await msg.edit_text(f"⬇️ Downloading:\n{title}")
+        await msg.edit_text(
+            f"⬇️ Downloading:\n{title}"
+        )
 
         ydl_opts = {
-            "format": "bestaudio",
+            "format": "140/bestaudio",
             "outtmpl": "downloads/%(title)s.%(ext)s",
             "quiet": True,
             "noplaylist": True,
+            "cookiefile": "cookies.txt",
+            "geo_bypass": True,
+            "extractor_args": {
+                "youtube": {
+                    "player_client": ["android"]
+                }
+            }
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -71,15 +93,21 @@ async def play(client, message: Message):
             file_path = ydl.prepare_filename(info)
 
         if not os.path.exists(file_path):
-            return await msg.edit_text("❌ Download failed")
+            return await msg.edit_text(
+                "❌ Download failed"
+            )
 
-        await msg.edit_text("📤 Uploading...")
+        await msg.edit_text("📤 Uploading Audio...")
 
         await message.reply_audio(
             audio=file_path,
             title=title,
             performer="YouTube",
-            caption=f"🎵 {title}"
+            caption=f"""
+🎵 {title}
+
+👑 @BeStChEaT_OwNeR
+"""
         )
 
         await msg.delete()
@@ -94,7 +122,6 @@ async def play(client, message: Message):
         await msg.edit_text(
             f"❌ Error:\n{e}"
         )
-
 
 print("✅ Music Bot Running")
 
