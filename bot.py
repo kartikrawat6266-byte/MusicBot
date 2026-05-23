7c48e719e5842feccd0f7b64a833baab95677aff
kartikrawat6266-byte <kartikrawat6266@gmail.com>
2026-05-23 22:03:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from youtube_search import YoutubeSearch
+from youtubesearchpython import VideosSearch
 import yt_dlp
 
 API_ID = 35140329
